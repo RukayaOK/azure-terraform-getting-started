9e644109fef43d4d677b70a3bc0095eddb1e8328
RukayaOK <34509633+RukayaOK@users.noreply.github.com>
2023-01-22 16:44:38 +0000

    add oidc

---
+++ 9_azure_oidc/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rok-terraform-rg-do-not-delete"
+    storage_account_name = "terrates901"
+    container_name       = "state"
+    key                  = "oidc.tfstate"
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+
+  use_oidc = true
+}
