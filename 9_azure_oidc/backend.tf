f0a889c7db2d4e74d545f252d856f132b4506dee
RukayaOK <34509633+RukayaOK@users.noreply.github.com>
2023-01-22 17:37:13 +0000

    use oidc

--- 9_azure_oidc/backend.tf
+++ 9_azure_oidc/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      source = "hashicorp/azurerm"
+      version = "3.30.0"
     }
   }
   backend "azurerm" {
